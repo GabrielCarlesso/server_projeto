4b514d08b7ca1c96c9d8303323e61f52318dd433
GabrielCarlesso <gcarlessox@gmail.com>
2023-06-18 19:34:10 -0300

    createToken

--- server.c
+++ server.c
@@ -4,12 +4,58 @@
 #include <ulfius.h>
 #include <locale.h>
 #include <orcania.h>
+#include <jansson.h>
 
 #define PORT 8082
 
 char *json;
 
 char * print_map(const struct _u_map * map);
+
+int callback_default (const struct _u_request * request, struct _u_response * response, void * user_data);
+int callback_get_with_params(const struct _u_request *request, struct _u_response *response, void *user_data);
+int createToken(const struct _u_request *request, struct _u_response *response, void *user_data);
+int callback_post(const struct _u_request *request, struct _u_response *response, void *user_data);
+
+/*
+ * main function
+ */
+int main(void) {
+  setlocale(LC_ALL, "");
+  char txt[] = "{\"Ip\": \"8.8.8.8\",\"Cow\": [{\"ID\": 1,	\"Localizacao\": [{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": -29.6928309,\"Longitude\": -53.8094293},{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636}]},{\"ID\": 2,\"Localizacao\": [{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636},{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636}]}]}";
+  json = txt;
+
+
+  struct _u_instance instance;
+
+  // Initialize instance with the port number
+  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+    fprintf(stderr, "Error ulfius_init_instance, abort\n");
+    return(1);
+  }
+
+  // Endpoint list declaration
+  ulfius_add_endpoint_by_val(&instance, "POST", "/post", NULL, 0, &callback_post, NULL);
+  ulfius_add_endpoint_by_val(&instance, "GET", "/createToken", NULL, 0, &createToken, NULL);
+  ulfius_add_endpoint_by_val(&instance, "GET", "/localization",NULL, 0, &callback_get_with_params, NULL);
+  ulfius_set_default_endpoint(&instance, &callback_default, NULL);// default_endpoint declaration
+
+  // Start the framework
+  if (ulfius_start_framework(&instance) == U_OK) {
+    printf("Start framework on port %d\n", instance.port);
+
+    // Wait for the user to press <enter> on the console to quit the application
+    getchar();
+  } else {
+    fprintf(stderr, "Error starting framework\n");
+  }
+  printf("End framework\n");
+
+  ulfius_stop_framework(&instance);
+  ulfius_clean_instance(&instance);
+
+  return 0;
+}
 
 int callback_get_with_params(const struct _u_request *request, struct _u_response *response, void *user_data) {
     const char *param1 = u_map_get(request->map_url, "startDate");
@@ -30,8 +76,53 @@ int callback_get_with_params(const struct _u_request *request, struct _u_respons
 
     return U_CALLBACK_CONTINUE;
 }
+
+
+int createToken(const struct _u_request *request, struct _u_response *response, void *user_data) {
+    const char *param1 = u_map_get(request->map_url, "Ip");
+    
+
+    /* Gerar o token */
+    
+    /* Gerar o token */
+    ulfius_set_string_body_response(response, 200, param1);
+    
+
+    return U_CALLBACK_CONTINUE;
+}
+
+
+
+/**
+ * Default callback function called if no endpoint has a match
+ */
+int callback_default (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  (void)(request);
+  (void)(user_data);
+  ulfius_set_string_body_response(response, 404, "Page not found, do what you want");
+  return U_CALLBACK_CONTINUE;
+}
+
 int callback_post(const struct _u_request *request, struct _u_response *response, void *user_data) {
+json_t *json_body = ulfius_get_json_body_request(request,NULL);
+    if (json_body != NULL) {
+        char *json_str = json_dumps(json_body, JSON_INDENT(2));
+        printf("JSON recebido:\n%s\n", json_str);
+        free(json_str);
+    } else {
+        printf("Falha ao obter o JSON do corpo da solicitação.\n");
+    }
+
+  return U_CALLBACK_CONTINUE;
+}
+
+
+
+/* Exemplo de post com body x-www-form-urlencoded */
+/*
+int callback_post_test (const struct _u_request * request, struct _u_response * response, void * user_data) {
   char * post_params = print_map(request->map_post_body);
+  printf("\nRecebido: %s\n", post_params);
   char * response_body = msprintf("Hello World!\n%s", post_params);
   (void)(user_data);
   ulfius_set_string_body_response(response, 200, response_body);
@@ -39,60 +130,6 @@ int callback_post(const struct _u_request *request, struct _u_response *response
   o_free(post_params);
   return U_CALLBACK_CONTINUE;
 }
-
-int createToken(const struct _u_request *request, struct _u_response *response, void *user_data) {
-    const char *param1 = u_map_get(request->map_url, "startDate");
-    const char *param2 = u_map_get(request->map_url, "endDate");
-
-    /* Gerar o token */
-    
-    /* Gerar o token */
-    ulfius_set_string_body_response(response, 200, json);
-    
-
-    return U_CALLBACK_CONTINUE;
-}
-
-/*
- * main function
- */
-int main(void) {
-  setlocale(LC_ALL, "");
-  char txt[] = "{\"Ip\": \"8.8.8.8\",\"Cow\": [{\"ID\": 1,	\"Localizacao\": [{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": -29.6928309,\"Longitude\": -53.8094293},{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636}]},{\"ID\": 2,\"Localizacao\": [{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636},{\"Horario\": \"2023-06-06 23:39:40.845\",\"Latitude\": 45.516,\"Longitude\": -122.636}]}]}";
-  json = txt;
-  // printf("json = %s\n", json);
-  struct _u_instance instance;
-
-  // Initialize instance with the port number
-  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
-    fprintf(stderr, "Error ulfius_init_instance, abort\n");
-    return(1);
-  }
-
-  // Endpoint list declaration
-  ulfius_add_endpoint_by_val(&instance, "POST", "/post", NULL, 0, &callback_post, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", "/createToken", NULL, 0, &createToken, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", "/teste" , "/localization", 0, &callback_get_with_params, NULL);
-
-  // Start the framework
-  if (ulfius_start_framework(&instance) == U_OK) {
-    printf("Start framework on port %d\n", instance.port);
-
-    // Wait for the user to press <enter> on the console to quit the application
-    getchar();
-  } else {
-    fprintf(stderr, "Error starting framework\n");
-  }
-  printf("End framework\n");
-
-  ulfius_stop_framework(&instance);
-  ulfius_clean_instance(&instance);
-
-  return 0;
-}
-
-
-
 
 char * print_map(const struct _u_map * map) {
   char * line, * to_return = NULL;
@@ -123,3 +160,4 @@ char * print_map(const struct _u_map * map) {
     return NULL;
   }
 }
+*/
